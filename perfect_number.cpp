6519a0813bef7ff95b783cfd00ffa969b721be2d
ASHOK KUMAR <62152494+ashok-2001@users.noreply.github.com>
2023-02-01 20:26:55 +0530

    leetcode

---
+++ perfect_number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int sum=1;
+        if(num==1) return false;
+        for(int i=2; i<=(num/2); i++){
+            if(num%i==0) sum=sum+i;
+        }
+        if(sum==num) return true;
+        return false;
+    }
+};
